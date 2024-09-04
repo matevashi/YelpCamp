63e9b8c14eb0818a8255e17b156e7d7db7e71e9c
matevashi <matevashi@outlook.jp>
2024-09-04 18:32:38 +0900

    ✨ 認可の追加

--- seeds/index.js
+++ seeds/index.js
@@ -25,6 +25,7 @@ const seedDB = async () => {
     const randomCityIndex = Math.floor(Math.random() * cities.length);
     const price = Math.floor(Math.random() * 2000) + 1000;
     const camp = new Campground({
+      author: "66cd84f8b0d57d63fc7e9415",
       location: `${cities[randomCityIndex].prefecture}${cities[randomCityIndex].city}`,
       title: `${sample(descriptors)}・${sample(places)}`,
       image: "https://picsum.photos/500",
